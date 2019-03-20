05dfa2b2fe663169383b75c69c9d6b2025bd6a04
papidre <andrealcalde2998@gmail.com>
2019-03-20 11:10:25 +0100

    b)

--- app/src/main/java/com/example/homersimpson_andrealcalde/MainActivity.java
+++ app/src/main/java/com/example/homersimpson_andrealcalde/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.homersimpson_andrealcalde;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView animacio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        animacio = findViewById(R.id.imageTitol);
+        AnimationDrawable anim = (AnimationDrawable) animacio.getDrawable();
+        anim.start();
+
     }
+
+
 }
