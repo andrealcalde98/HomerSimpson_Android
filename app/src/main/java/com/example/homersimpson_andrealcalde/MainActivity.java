6fb530e3bc001108d6b9c315fb979e3b5eca91a1
papidre <andrealcalde2998@gmail.com>
2019-03-20 11:44:29 +0100

    c)

--- app/src/main/java/com/example/homersimpson_andrealcalde/MainActivity.java
+++ app/src/main/java/com/example/homersimpson_andrealcalde/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView animacio;
+    ImageView animacio, homer, blau, verd, vermell, donut, ull;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +19,41 @@ public class MainActivity extends AppCompatActivity {
         animacio = findViewById(R.id.imageTitol);
         AnimationDrawable anim = (AnimationDrawable) animacio.getDrawable();
         anim.start();
+        blau = findViewById(R.id.imageBlau);
+        blau.setVisibility(View.INVISIBLE);
 
+        verd = findViewById(R.id.imageVerd);
+        verd.setVisibility(View.INVISIBLE);
+
+        vermell = findViewById(R.id.imageVermell);
+        vermell.setVisibility(View.INVISIBLE);
+
+        donut = findViewById(R.id.imageDonut);
+        donut.setVisibility(View.INVISIBLE);
+
+        ull = findViewById(R.id.imageUll);
+        ull.setVisibility(View.INVISIBLE);
+
+        homer = findViewById(R.id.imageHomer);
+        homer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (blau.getVisibility() == View.INVISIBLE) {
+                    blau.setVisibility(View.VISIBLE);
+                    verd.setVisibility(View.VISIBLE);
+                    vermell.setVisibility(View.VISIBLE);
+                    donut.setVisibility(View.VISIBLE);
+                    ull.setVisibility(View.VISIBLE);
+                } else {
+                    blau.setVisibility(View.INVISIBLE);
+                    verd.setVisibility(View.INVISIBLE);
+                    vermell.setVisibility(View.INVISIBLE);
+                    donut.setVisibility(View.INVISIBLE);
+                    ull.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
     }
 
 
